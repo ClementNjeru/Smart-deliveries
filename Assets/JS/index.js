eb2fb7bb8a0c05ba11db60e9c3aa111dd5d879c7
ClementNjeru <njeruclement36@gmail.com>
2022-06-22 23:24:56 +0300

    modifies the JavaScript DOMmanupulation

--- Assets/JS/index.js
+++ Assets/JS/index.js
@@ -1,20 +1,58 @@
-document.getElementById('search').addEventListener('click',getCocktails)
-function getCocktails(){
-    // const search = document.getElementById ('search')
-fetch(`http://localhost:3000/drinks`)
-.then(resp => resp.json())
-.then ( data => {
-    let output = "";
-    data.forEach(drink => { 
-        output =+ 
-        // output +
-        `
-        <div class="cock-tails" id="${drink.idDrink}">
-        <img src="${drink.strDrinkThumb}" alt="drink">
-        <h3>${drink.strDrink}</h3>
-    </div>
-        `
-        
+// document.getElementById('search').addEventListener('click', getCocktails)
+
+// function getCocktails() {
+//     // const search = document.getElementById ('search')
+//     fetch(`http://localhost:3000/drinks`)
+//         .then(resp => resp.json())
+//         .then(data => {
+//             console.log(data)
+//             let html = "";
+//             data.forEach(drink => {
+//                 // output =+
+//                 //     // output +
+//              html += `
+//         <div class="drinks" id = "drink">
+//         <img src="${data.strDrinkThumb}"alt="drink">
+//         <h3>${data.strDrink}</h3>
+//     </div>  
+//     `
+//             });
+//         })
+// }
+// getCocktails()
+
+
+const renderPerson = (person) => {
+    let card = document.createElement("li");
+    card.className = "card";
+    card.innerHTML = `
+    <h2>Name: ${person.name}</h2>
+      <div class="content hidden">
+          <p>Mass: ${person.mass}Kgs</p>
+          <p>Hair Color: ${person.hair_color}</p>
+          <p>Skin Color: ${person.skin_color}</p>
+      </div>
+      <button class="btn">More Info</button>`;
+    document.getElementById("container").appendChild(card);
+    document.querySelectorAll(".btn").forEach((btn) => {
+      btn.addEventListener("click", () => {
+        document.querySelectorAll(".content").forEach((item) => {
+          item.classList.toggle("hidden");
+          console.log("clicked");
+        });
+      });
     });
-})}
-getCocktails()
+  };
+  
+  //2. Async Await
+  const getCocktails = async () => {
+    const response = await fetch("http://localhost:3000/drinks");
+    const data = await response.json();
+    console.log(data);
+    const results = data.results;
+  
+    results.map((drinks) => getCocktails(drinks));
+    console.log(results);
+  };
+  
+  getCocktails();
