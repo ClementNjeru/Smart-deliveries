b61e5cc6bcf6bb081780a4e14ca53eaee177fd71
ClementNjeru <njeruclement36@gmail.com>
2022-06-23 15:57:37 +0300

    rectifies the Javascript file

--- Assets/JS/index.js
+++ Assets/JS/index.js
@@ -4,34 +4,10 @@
     const data = await response.json();
     console.log(data);
     const results = data.results;
-  
-    results.map((drinks) => getCocktails(drinks));
-    console.log(results);
   };
   
   getCocktails();
 
 //   elements
-const checkDrink = (drinks) => {
-    let card = document.createElement("li");
-    card.className = "drinks";
-    
-    card.innerHTML =  html=+`
-    <h2>Name: ${drinks.strDrink}</h2>
-    <div class = "dname">
-        <p>strDrink: ${drinks.strDrink}<p/>
-        <p>strDrinkThumb: ${drinks.strDrinkThumb}<p/>
-    <div/>
-    `
-    document.getElementById("drinks").appendChild(card);
-    document.querySelectorAll(".btn").forEach((btn) => {
-      btn.addEventListener("click", () => {
-        document.querySelectorAll(".content").forEach((item) => {
-          item.classList.toggle("hidden");
-          console.log("clicked");
-        });
-      });
-    });
 
-}
-checkDrink();
+
