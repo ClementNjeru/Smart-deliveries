02afc1ce66a571bf3443b2e2aef099a14de4cf64
ClementNjeru <njeruclement36@gmail.com>
2022-06-23 18:17:41 +0300

    modifies the Javascript file by fetching data from an external API

--- Assets/JS/index.js
+++ Assets/JS/index.js
@@ -1,13 +1,22 @@
   //. initializing
-  const getCocktails = async () => {
-    const response = await fetch("http://localhost:3000/drinks");
-    const data = await response.json();
-    console.log(data);
-    const results = data.results;
-  };
-  
-  getCocktails();
+  fetch('http://localhost:3000/drinks')
+    .then((data) => {
+      return data.json();
+    })
+    .then((completeddata) => {
+      let data1 = "";
+      completeddata.map((value) => {
+        data1 += `
+      <div id="drinks">
+        <h3>${value.strDrink}</h3>
+        <img src=${value.strDrinkThumb} alt="img" class ="images">
+        <p> ${value.idDrink}</p>
+      </div>
+    `
+      });
+      document.getElementById("drink").innerHTML = data1;
 
-//   elements
-
-
+    }).catch((Error) => {
+      console.log(Error)
+    })
+  //   elements
